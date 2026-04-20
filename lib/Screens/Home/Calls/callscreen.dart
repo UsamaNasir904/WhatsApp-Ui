2e8ae9c216cf0c9a9f7d990ec7bc6b615b6147ce
UsamaNasir904 <usama.nasir.stud.usama.nasir.904@gmail.com>
2026-04-20 11:28:00 +0500

    CallScreen Complate

--- lib/Screens/Home/Calls/callscreen.dart
+++ lib/Screens/Home/Calls/callscreen.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
 
+import '../../../Widgets/uihelper.dart';
+
 class CallScreen extends StatelessWidget{
+  var callContents=[
+    {
+      "image": "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?semt=ais_hybrid&w=740&q=80",
+      "name": "Ali",
+      "calltime": "15 mins ago",
+    },
+    {
+      "image": "https://img.freepik.com/free-photo/young-beautiful-woman-pink-warm-sweater-natural-look-smiling-portrait-isolated-long-hair_285396-896.jpg?semt=ais_hybrid&w=740&q=80",
+      "name": "Amna",
+      "calltime": "20 mins ago",
+    }];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calls"),
-        centerTitle: true,
-      ),
+      body: Column(
+        children: [
+          SizedBox(height: 20,),
+          Row(
+            children: [
+              SizedBox(width: 20,),
+              UiHelper.CustomText(text: "Recent Calls", height: 16,fontweight: FontWeight.bold, color: Colors.black),
+            ],
+          ),
+          SizedBox(height: 10,),
+          Expanded(
+            child: ListView.builder(itemBuilder: (context,index){
+              return ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage(callContents[index]["image"].toString()),
+                ),
+                title: UiHelper.CustomText(text: callContents[index]["name"].toString(), height: 16,fontweight: FontWeight.bold,color: Colors.black),
+                subtitle: UiHelper.CustomText(text: callContents[index]["calltime"].toString(), height: 14),
+                trailing: IconButton(onPressed: (){
+
+                }, icon: Icon(Icons.call,color: Color(0XFF008069))),
+              );
+            },itemCount: callContents.length,),
+          )
+        ],
+      )
     );
   }
 
--- lib/Screens/Home/Status/statusscreen.dart
+++ lib/Screens/Home/Status/statusscreen.dart
@@ -1,12 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/Widgets/uihelper.dart';
 
 class StatusScreen extends StatelessWidget{
+  var statusContents=[
+    {
+    "image": "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?semt=ais_hybrid&w=740&q=80",
+    "name": "Ali",
+    "statustime": "15 mins ago",
+     },
+    {
+      "image": "https://img.freepik.com/free-photo/young-beautiful-woman-pink-warm-sweater-natural-look-smiling-portrait-isolated-long-hair_285396-896.jpg?semt=ais_hybrid&w=740&q=80",
+      "name": "Amna",
+      "statustime": "20 mins ago",
+    }];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Status"),
-        centerTitle: true,
+      body: Column(
+        children: [
+          SizedBox(height: 10,),
+          Row(
+            children: [
+              SizedBox(width: 10,),
+              UiHelper.CustomText(text: "Status", height: 20,fontweight: FontWeight.bold, color: Colors.black)
+            ],
+            ),
+          SizedBox(height: 10,),
+          ListTile(
+            leading: Padding(padding: const EdgeInsets.only(left: 5),
+            child: Stack(
+              children: [
+                CircleAvatar(
+                  radius: 25,
+                  backgroundImage: NetworkImage("https://st3.depositphotos.com/9998432/13335/v/450/depositphotos_133352010-stock-illustration-default-placeholder-man-and-woman.jpg"),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: CircleAvatar(
+                    radius: 10,
+                    backgroundColor: Color(0XFF008069),
+                    child: Center(child: Icon(Icons.add,color: Colors.white,size: 15,)),
+                  ),
+                )
+              ],
+            ),
+            ),
+            title: UiHelper.CustomText(text: "My Status", height: 16,fontweight: FontWeight.bold),
+            subtitle: UiHelper.CustomText(text: "Tap to add status update", height: 14),
+          ),
+          SizedBox(height: 20,),
+          Padding(
+            padding: const EdgeInsets.only(left: 20,right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                UiHelper.CustomText(text: "Recent updates", height:15),
+                Icon(Icons.arrow_drop_down,color: Color(0XFF5E5E5E))
+              ],
+            ),
+          ),
+          SizedBox(height: 10,),
+          Expanded(
+            child: ListView.builder(itemBuilder: (context,index){
+              return ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage(statusContents[index]["image"].toString()),
+                ),
+                title: UiHelper.CustomText(text: statusContents[index]["name"].toString(), height: 16,fontweight: FontWeight.bold),
+                subtitle: UiHelper.CustomText(text: statusContents[index]["statustime"].toString(), height: 14),
+              );
+            },itemCount: statusContents.length,),
+          )
+        ],
       ),
     );
   }
